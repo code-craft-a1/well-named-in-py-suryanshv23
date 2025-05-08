00a67daeb104658f4c052a8fe9a83e47acb0cf05
suryanshv23 <47254107+suryanshv23@users.noreply.github.com>
2025-05-08 21:01:25 +0000

    modular code

--- Tests/unit_tests.py
+++ Tests/unit_tests.py
@@ -1,4 +1,4 @@
-from utils.helper import get_color_from_pair_number, get_pair_number_from_color
+from utils.helpers import get_color_from_pair_number, get_pair_number_from_color
 
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
